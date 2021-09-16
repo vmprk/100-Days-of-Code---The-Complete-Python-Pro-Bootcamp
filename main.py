f88e5522367b0e75d2eb639ec542a1fb3d24a3f7
VMPR K <90391928+vmprk@users.noreply.github.com>
2021-09-16 19:01:10 +0000

    day6

--- main.py
+++ main.py
@@ -2,11 +2,12 @@
 number = int(input("Which number do you want to check? "))
 # 🚨 Don't change the code above 👆
 
-#First *fork* your copy. Then copy-paste your code below this line 👇
+
 #Finally click "Run" to execute the tests
 
 if number % 2 == 0:
   print("This is an even number.")
+
 else:
   print("This is an odd number.")
 
