392fa99a7c2b269b03fe30b4092aae9acd3ffca4
VMPR K <90391928+vmprk@users.noreply.github.com>
2021-09-17 18:57:53 +0000

    day7

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ number = int(input("Which number do you want to check? "))
 # 🚨 Don't change the code above 👆
 
 
-#Finally click "Run" to execute the tests
-
 if number % 2 == 0:
   print("This is an even number.")
 
